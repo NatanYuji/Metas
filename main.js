5142d00487513472980926c6429b43a0d0f15dc0
NatanYuji <134072159+NatanYuji@users.noreply.github.com>
2024-05-08 10:49:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -20,25 +20,37 @@ const tempoObjetivo2 = new Date("2025-02-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-21T00:00:00");
 const tempoObjetivo4 = new Date("2024-11-10T00:00:00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-contadores[0].textContent = calcularTempo(tempoObjetivo1);
-contadores[1].textContent = calcularTempo(tempoObjetivo2);
-contadores[2].textContent = calcularTempo(tempoObjetivo3);
-contadores[3].textContent = calcularTempo(tempoObjetivo4);
 
-function calcularTempo(tempoObjetivo)
-{
+function calcularTempo(tempoObjetivo) {
     let tempoAtual = new Date();
-    let tempofinal =tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempofinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let tempofinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempofinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempofinal > 0) {
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    }else{
+        return "Prezo Finalizado";
+    }
 
-    
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+
+function atualizarCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calcularTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro() {
+    atualizarCronometro();
+    setInterval(atualizarCronometro, 1000);
+}
+
+
